91e7de2be03bdc2135859ff32672ecd743745f92
luislutz <80843997+luislutz@users.noreply.github.com>
2021-12-07 19:57:50 +0100

    Update radio_scanner.py

--- software/programs/radio_scanner.py
+++ software/programs/radio_scanner.py
@@ -1,6 +1,8 @@
 from europi import *
 from time import sleep
 from random import randint
+import machine
+
 
 
 def remap_knob(): 
@@ -84,4 +86,45 @@ while True:
     
     if display_time < 1:
         display_mapping(knob_mapping)
-    oled.show()
+        
+    oled.show()    
+        
+
+    
+    
+    
+    #include this at the end of your loop to reboot Pi with both Buttons Pressed for longer
+    
+    #Dont forget to add the libraryy at the top: import machine
+    
+    millis = ticks_ms()
+    Button1 = b1.value()
+    Button2 = b2.value()
+       
+    
+    if Button1 == 1 and Button1_toggle == 0:
+        Button1_toggle = 1
+        Button1_time = millis
+    if Button1 == 0:
+        Button1_toggle = 0
+    
+    if Button2 == 1 and Button2_toggle == 0:
+        Button2_toggle = 1
+        Button2_time = millis
+    if Button2 == 0:
+        Button2_toggle = 0
+    
+    if (Button1 == 1 and Button2 == 1 and millis-Button1_time > 1500 and millis-Button2_time > 1500):     
+        oled.centre_text('Reboot...')  
+        oled.show()
+        cv1.voltage(0)
+        cv2.voltage(0)
+        cv3.voltage(0)
+        cv4.voltage(0)
+        cv5.voltage(0)
+        cv6.voltage(0)
+        sleep(1)
+        machine.reset()
+    
+    
+
